7bec2db390aa4e2b1bcd927ab4aaec2a6ab36b9e
Francesco Zorzi <frensinik89@yahoo.it>
2025-08-30 16:02:31 +0200

    chore: change env var

--- sd-backend/SplitDuo.Core/Options/Setup/AppOptionsSetup.cs
+++ sd-backend/SplitDuo.Core/Options/Setup/AppOptionsSetup.cs
@@ -6,7 +6,7 @@ public class AppOptionsSetup : IConfigureOptions<AppOptions>
 {
     public void Configure(AppOptions options)
     {
-        options.Environment = Environment.GetEnvironmentVariable("PB_ENVIRONMENT") ?? "Development";
-        options.BaseUrl = Environment.GetEnvironmentVariable("PB_BASE_URL") ?? "http://localhost:3000";
+        options.Environment = Environment.GetEnvironmentVariable("SD_ENVIRONMENT") ?? "Development";
+        options.BaseUrl = Environment.GetEnvironmentVariable("SD_BASE_URL") ?? "http://localhost:3000";
     }
 }
--- sd-backend/SplitDuo.Core/Options/Setup/DatabaseOptionsSetup.cs
+++ sd-backend/SplitDuo.Core/Options/Setup/DatabaseOptionsSetup.cs
@@ -6,10 +6,10 @@ public class DatabaseOptionsSetup : IConfigureOptions<DatabaseOptions>
 {
     public void Configure(DatabaseOptions options)
     {
-        options.Host = Environment.GetEnvironmentVariable("PB_DB_HOST") ?? "localhost";
-        options.Port = Environment.GetEnvironmentVariable("PB_DB_PORT") ?? "5432";
-        options.Database = Environment.GetEnvironmentVariable("PB_DB_NAME") ?? "pocket-barber";
-        options.Username = Environment.GetEnvironmentVariable("PB_DB_USERNAME") ?? "pocket-barber";
-        options.Password = Environment.GetEnvironmentVariable("PB_DB_PASSWORD") ?? "pocket-barber";
+        options.Host = Environment.GetEnvironmentVariable("SD_DB_HOST") ?? "localhost";
+        options.Port = Environment.GetEnvironmentVariable("SD_DB_PORT") ?? "5432";
+        options.Database = Environment.GetEnvironmentVariable("SD_DB_NAME") ?? "splitduo";
+        options.Username = Environment.GetEnvironmentVariable("SD_DB_USERNAME") ?? "splitduo";
+        options.Password = Environment.GetEnvironmentVariable("SD_DB_PASSWORD") ?? "splitduo";
     }
 }
