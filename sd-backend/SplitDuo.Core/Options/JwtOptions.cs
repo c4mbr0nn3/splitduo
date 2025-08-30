ee85f8789c9368964aedce82fa1e00c501e0b8a8
Francesco Zorzi <frensinik89@yahoo.it>
2025-08-30 18:11:02 +0200

    fix: make SecretKey nullable in JwtOptions

--- sd-backend/SplitDuo.Core/Options/JwtOptions.cs
+++ sd-backend/SplitDuo.Core/Options/JwtOptions.cs
@@ -3,7 +3,7 @@ namespace SplitDuo.Core.Options;
 public class JwtOptions
 {
     public const string SectionName = "Jwt";
-    public required string SecretKey { get; set; }
+    public string? SecretKey { get; set; }
     public required string Issuer { get; set; }
     public required string Audience { get; set; }
     public required int Expires { get; set; }
