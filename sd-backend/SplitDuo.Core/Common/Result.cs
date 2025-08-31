03e3af88f98729675671ffe7e5a00e2c90e24611
Francesco Zorzi <frensinik89@yahoo.it>
2025-08-31 12:01:22 +0200

    chore: stuff

--- sd-backend/SplitDuo.Core/Common/Result.cs
+++ sd-backend/SplitDuo.Core/Common/Result.cs
@@ -8,7 +8,7 @@ public class Result<T>
     public bool IsFailure => !IsSuccess;
     public T? Value { get; private set; }
     public string Error { get; private set; } = "";
-    public HttpStatusCode StatusCode { get; private set; } = HttpStatusCode.OK;
+    public HttpStatusCode StatusCode { get; private set; }
 
     private Result(T value, HttpStatusCode statusCode = HttpStatusCode.OK)
     {
@@ -50,7 +50,7 @@ public class Result
 {
     public bool IsSuccess { get; private set; }
     public bool IsFailure => !IsSuccess;
-    public string Error { get; private set; } = "";
+    public string Error { get; private set; }
     public HttpStatusCode StatusCode { get; private set; }
 
     private Result(bool isSuccess, string error = "", HttpStatusCode statusCode = HttpStatusCode.OK)
