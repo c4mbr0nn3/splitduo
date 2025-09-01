2b6138d398373a3ebcd1e74219d71ab63e1713fd
Francesco Zorzi <frensinik89@yahoo.it>
2025-09-01 15:13:26 +0200

    style: lint

--- sd-frontend/nuxt.config.ts
+++ sd-frontend/nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/ui',
-    '@nuxt/eslint'
+    '@nuxt/eslint',
   ],
+  devtools: { enabled: true },
 
   css: ['~/assets/css/main.css'],
 
-  compatibilityDate: '2025-07-16'
+  compatibilityDate: '2025-07-16',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
