cb01326b64f575ec16fcee4a7fafcf4fd179cc0f
Francesco Zorzi <frensinik89@yahoo.it>
2026-02-22 19:14:36 +0100

    feat: db migrations

--- sd-backend/SplitDuo.Core/Migrations/20260222181343_Add2FASecurityHardening.Designer.cs
+++ sd-backend/SplitDuo.Core/Migrations/20260222181343_Add2FASecurityHardening.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SplitDuo.Core.Persistence;
@@ -11,9 +12,11 @@ using SplitDuo.Core.Persistence;
 namespace SplitDuo.Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260222181343_Add2FASecurityHardening")]
+    partial class Add2FASecurityHardening
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ sd-backend/SplitDuo.Core/Migrations/20260222181343_Add2FASecurityHardening.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SplitDuo.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add2FASecurityHardening : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "last_used_totp_time_step",
+                table: "users",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_used_totp_time_step",
+                table: "users");
+        }
+    }
+}
